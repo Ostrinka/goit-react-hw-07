e557180172ed02fd8a2814933e391505dc45f94f
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-06-07 18:49:41 +0300

    del Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,33 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import contactSlice from "./contactsSlice";
-import filterSlice from "./filtersSlice";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-} from "redux-persist/es/constants";
+import { configureStore } from "@reduxjs/toolkit";
+import contactsReducer from "./contactsSlice";
+import filtersReducer from "./filtersSlice";
+// import storage from "redux-persist/lib/storage";
+// import { persistReducer, persistStore } from "redux-persist";
+// import {
+//   FLUSH,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+//   REHYDRATE,
+// } from "redux-persist/es/constants";
 
-const rootReducer = combineReducers({
-  contacts: contactSlice,
-  filter: filterSlice,
-});
+// const rootReducer = combineReducers({
+//   contacts: contactSlice,
+//   filter: filterSlice,
+// });
 
-const persistConfig = {
-  key: "contacts",
-  storage,
-};
+// const persistConfig = {
+//   key: "contacts",
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  }
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
