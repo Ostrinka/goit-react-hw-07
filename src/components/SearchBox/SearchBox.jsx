f9982e405bf6a5bf363eb4d67db513cdb41b392e
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-06-07 20:00:07 +0300

    add contactsOps

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { setFilter, selectFilter } from '../../redux/filtersSlice';
+import { changeFilter, selectFilter } from '../../redux/filtersSlice';
 import css from './SearchBox.module.css';
 
 export default function SearchBox() {
@@ -7,7 +7,7 @@ export default function SearchBox() {
   const filter = useSelector(selectFilter);
 
   const handleChange = (event) => {
-    dispatch(setFilter(event.target.value));
+    dispatch(changeFilter(event.target.value));
   };
   
   return (
