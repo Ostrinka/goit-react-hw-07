4a3fd88bf8ee77fbcc80c239afa4be947e24ead7
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-06-07 20:12:06 +0300

    in process

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,14 @@
 import Contact from '../Contact/Contact.jsx'
 import { useSelector } from 'react-redux';
-import { filteredContacts } from '../../redux/filtersSlice';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 import css from './ContactList.module.css';
 
 export default function ContactList() {
-  const contacts = useSelector(filteredContacts);
-  
+  // const contacts = useSelector(selectContacts);
+  const filteredContacts = useSelector(selectFilteredContacts);
   return (
     <ul className={css.menu}>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <li className={css.list} key={contact.id} >
           <Contact contact={contact} />
         </li>
